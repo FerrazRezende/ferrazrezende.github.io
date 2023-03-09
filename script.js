a1f729986cafb4255fd4d42800ef6efab5bc19be
Matheus Ferraz <107080615+FerrazRezende@users.noreply.github.com>
2023-03-08 21:31:02 -0300

    Finalizando api da nasa
    Finalizando consumo de api com fetch

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 async function carregarImagem(){
-    try {
+  
         var imgNasa = await fetch('https://api.nasa.gov/planetary/apod?api_key=Us9V4qF23cCiIx08S6XESjjkN1VZtNWkFLydrFA4')
         var imgConvertida = await imgNasa.json();
         let imgUrl = imgConvertida.hdurl;
-        var autorFoto = document.getElementById('autor')
-        var dataFoto = document.getElementById('data')
-        var copyright = imgConvertida.copyright
+        var autor = imgConvertida.copyright
         var data = imgConvertida.date
-        var btn = document.getElementById('nasa-btn');
-        btn.addEventListener("click", () => {
-        document.getElementById('img-nasa').setAttribute("src", imgUrl),
-        document.getElementById('nasa-btn').setAttribute("id", "fechar-imagem"),
-        autorFoto.innerHTML = "Autor: "+ copyright
-        dataFoto.innerHTML = "Data: " + data
-        window.scrollBy(800, 1300),
-        btn.textContent = "Fechar imagem",
-        console.log(imgConvertida)
-        }
-        )
+        html = `
+        <div id="slideup">
+        <img src="${imgUrl}" class="" id="img-nasa" width="380px">
+        <div class="tabs is-centered is-medium card div-img">
+        <div class="card-content">
+          <div class="content">
+            <p id="autor">Créditos: ${autor}</p>
+            <p id="data">Data: ${data}</p>
+          </div>
+        </div>
+      </div>
+      </div>
+      `
+}
 
-        if(imgConvertida.erro) {
-            throw Error("A Imagem não carregou!");
-        }
-    } catch (erro){
-        console.log(erro)
-    }
+function abrirImagem(){
+  if ($("#slideup").length){
+    alert("Imagem já adicionada")
+  }
+  else {
+    $("#img-div" ).append(html);
+    window.scrollBy(800, 1300);
+  }
+}
+function fecharImagem(){
+    $("#slideup").remove();
 }
 
 
-
-
-
 carregarImagem()
-
+abrirImagem()
+fecharImagem()
